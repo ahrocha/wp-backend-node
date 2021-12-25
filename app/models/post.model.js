b57eb3db6f1afdccf5ba43a7da0859d6df21e5ca
Andrey Hurpia da Rocha <ahrocha@gmail.com>
2021-12-25 10:25:51 -0300

    Fixing indentation

--- app/models/post.model.js
+++ app/models/post.model.js
@@ -10,10 +10,11 @@ const Post = function(post) {
 	this.tablePrefix = config.WP_TABLE_PREFIX
 };
 
-
 Post.findById = (postId, result) => {
 	
-	sql.query("SELECT ID, post_title, post_date, post_name, post_content FROM 541Pib644_posts WHERE ID = ${postId} AND post_status = 'publish' ", (err, res) => {
+	sql.query(`
+			SELECT ID, post_title, post_date, post_name, post_content 
+			FROM '${this.tablePrefix}'posts WHERE ID = ${postId} `, (err, res) => {
 		if (err) {
 			console.log("error", err);
 			result(err, null);
@@ -31,10 +32,11 @@ Post.findById = (postId, result) => {
 	});
 };
 
-
-
 Post.findByName = (postName, result) => {
-	sql.query(`SELECT ID, post_title, post_date, post_name, post_content FROM '${this.tablePrefix}'posts WHERE post_name = '${postName}' AND post_status = 'publish' LIMIT 1 `, (err, res) => {
+	sql.query(`
+		SELECT ID, post_title, post_date, post_name, post_content 
+		FROM '${this.tablePrefix}'posts WHERE post_name = '${postName}' 
+		AND post_status = 'publish' LIMIT 1 `, (err, res) => {
 		if (err) {
 			console.log("error", err);
 			result(err, null);
@@ -52,28 +54,23 @@ Post.findByName = (postName, result) => {
 	});
 };
 
-
 Post.getAll = result => {
 	sql.query(`
-	-- SELECT ID, post_title, post_date, post_name, post_status 
-	-- FROM '${this.tablePrefix}'posts 
-	-- WHERE post_status = 'publish' 
-	--   ORDER BY post_date DESC 
-	SELECT 
-		p.ID, p.post_title, p.post_date, p.post_name,
-		p.post_status, p.post_excerpt,
-		p.post_modified, p.guid,
-		u.display_name ,
-		(SELECT guid FROM '${this.tablePrefix}'posts ppp
-		LEFT JOIN '${this.tablePrefix}'postmeta wp ON wp.meta_value = ppp.ID
-		WHERE wp.post_id = p.ID AND wp.meta_key = '_thumbnail_id' AND ppp.post_status = 'inherit'
-		LIMIT 1
-		) as image
-	FROM '${this.tablePrefix}'posts p
-	LEFT JOIN '${this.tablePrefix}'users u ON p.post_author = u.ID
-	WHERE p.post_status = 'publish'
-	ORDER BY ID DESC 
-	LIMIT 10 ; `, (err, res) => {
+		SELECT 
+			p.ID, p.post_title, p.post_date, p.post_name,
+			p.post_status, p.post_excerpt,
+			p.post_modified, p.guid,
+			u.display_name ,
+			(SELECT guid FROM '${this.tablePrefix}'posts ppp
+			LEFT JOIN '${this.tablePrefix}'postmeta wp ON wp.meta_value = ppp.ID
+			WHERE wp.post_id = p.ID AND wp.meta_key = '_thumbnail_id' AND ppp.post_status = 'inherit'
+			LIMIT 1
+			) as image
+		FROM '${this.tablePrefix}'posts p
+		LEFT JOIN '${this.tablePrefix}'users u ON p.post_author = u.ID
+		WHERE p.post_status = 'publish'
+		ORDER BY ID DESC 
+		LIMIT 10 ; `, (err, res) => {
 		if (err) {
 			console.log("error", err);
 			result(err, null);
